1b55d27540d9001f229e69d1c209b9630876041b
nix.arif <nix.arif@gmail.com>
2022-01-14 14:27:56 +0800

    Problem when state ListItem

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,10 +1,23 @@
-import { fetchItemList } from '../redux';
+import { fetchItemList, insertItem } from '../redux';
 import { connect } from 'react-redux';
+import { useState } from 'react';
 
 const Form = (props) => {
+	const { insertItem } = props;
+
+	const [inputValue, setInputValue] = useState('');
+
+	const handleInputChange = (e) => {
+		setInputValue(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		insertItem(inputValue);
+	};
 	return (
 		<div className="jumbotron p-3 shadow-sm">
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div className="d-flex align-items-center">
 					<input
 						autoFocus
@@ -12,6 +25,8 @@ const Form = (props) => {
 						className="form-control mr-3"
 						type="text"
 						style={{ flex: 1 }}
+						value={inputValue}
+						onChange={handleInputChange}
 					/>
 					<button className="btn btn-primary">Add New Item</button>
 				</div>
@@ -28,6 +43,7 @@ const mapStateToProp = (state) => {
 
 const mapDispatchToProp = (dispatch) => {
 	return {
+		insertItem: (text) => dispatch(insertItem(text)),
 		fetchItemList: () => dispatch(fetchItemList()),
 	};
 };
--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,34 +1,41 @@
-import { useEffect } from "react";
-import { connect } from "react-redux";
-import { fetchItemList } from "../redux";
-import Item from "./Item";
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchItemList } from '../redux';
+import Item from './Item';
 
 const ListItem = (props) => {
-  const { loading, items, error, fetchItemList } = props;
+	console.log('render ListItem');
+	const { loading, items, error, fetchItemList } = props;
 
-  useEffect(() => {
-    fetchItemList();
-  }, []);
+	useEffect(() => {
+		console.log('run');
+		fetchItemList();
+	}, []);
 
-  return (
-    <ul className="list-group pb-5">
-      {items.map((item) => (
-        <Item key={item._id} item={item} />
-      ))}
-    </ul>
-  );
+	console.log('items:', items);
+
+	////////////////////////////////////////////////////////////
+	////////////////Double check items array////////////////////
+
+	return (
+		<ul className="list-group pb-5">
+			{/* {items.map((item) => (
+				<Item key={item._id} item={item} />
+			))} */}
+		</ul>
+	);
 };
 
 const mapStateToProp = (state) => {
-  return {
-    ...state,
-  };
+	return {
+		...state,
+	};
 };
 
 const mapDispatchToProp = (dispatch) => {
-  return {
-    fetchItemList: () => dispatch(fetchItemList()),
-  };
+	return {
+		fetchItemList: () => dispatch(fetchItemList()),
+	};
 };
 
 export default connect(mapStateToProp, mapDispatchToProp)(ListItem);
